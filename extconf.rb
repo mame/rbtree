ab88e2a20ef81c14627d7395d2286d2a69231663
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-02-12 21:59:19 +0900

    Drop ruby 2.0 and earlier
    Already using `RB_BLOCK_CALL_FUNC_ARGLIST` that has been defined since
    ruby 2.1.

--- extconf.rb
+++ extconf.rb
@@ -5,15 +5,6 @@ if enable_config('debug')
 else
   $defs << '-DNDEBUG'
 end
-have_header('ruby/version.h')
-have_func('rb_exec_recursive', 'ruby.h')
-have_func('rb_exec_recursive_paired', 'ruby.h')
-have_func('rb_proc_lambda_p', 'ruby.h')
-have_func('rb_ary_resize', 'ruby.h')
-have_func('rb_obj_hide', 'ruby.h')
 have_func('rb_safe_level', 'ruby.h')
 have_func('rb_cData', 'ruby.h')
-if Hash.method_defined?(:flatten)
-  $defs << '-DHAVE_HASH_FLATTEN'
-end
 create_makefile('rbtree')
